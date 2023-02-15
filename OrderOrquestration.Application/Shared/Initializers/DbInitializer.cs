9f31122da7c43fccae0e1e70b234e9aee9a09498
joelalvesoliver <joel.alves.oliver@gmail.com>
2023-02-15 20:26:23 -0300

    Initialize data in DB

--- OrderOrquestration.Application/Shared/Initializers/DbInitializer.cs
+++ OrderOrquestration.Application/Shared/Initializers/DbInitializer.cs
@@ -1,4 +1,5 @@
 ﻿using OrderOrquestration.Application.Shared.Context;
+using OrderOrquestration.Application.Shared.Domain.Entities;
 
 namespace OrderOrquestration.Application.Shared.Initializers
 {
@@ -7,6 +8,22 @@ namespace OrderOrquestration.Application.Shared.Initializers
         public static void Initialize(ApplicationDbContext context)
         {
             context.Database.EnsureCreated();
+
+            context.Actions.Add(new ActionConfiguration { Description = "Enviar E-mail" });
+            context.Actions.Add(new ActionConfiguration { Description = "Criar remessa" });
+            context.Actions.Add(new ActionConfiguration { Description = "Criar remessa duplicada" });
+            context.Actions.Add(new ActionConfiguration { Description = "Comissao agente" });
+            context.Actions.Add(new ActionConfiguration { Description = "Upgrade associação" });
+            context.Actions.Add(new ActionConfiguration { Description = "Criar associação" });
+
+            context.ItemConfigurations.Add(new ItemConfiguration { ActionId = 1, Product = "Livro" });
+            context.ItemConfigurations.Add(new ItemConfiguration { ActionId = 5, Product = "upgrade associação" });
+            context.ItemConfigurations.Add(new ItemConfiguration { ActionId = 1, Product = "upgrade associação" });
+            context.ItemConfigurations.Add(new ItemConfiguration { ActionId = 6, Product = "criar associacao" });
+            context.ItemConfigurations.Add(new ItemConfiguration { ActionId = 1, Product = "criar associacao" });
+            context.ItemConfigurations.Add(new ItemConfiguration { ActionId = 4, Product = "livro" });
+
+            context.SaveChanges();
         }
     }
 }
