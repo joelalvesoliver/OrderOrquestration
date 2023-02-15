172394da7b6b4b7f7df60f889122138e70cd1933
joelalvesoliver <joel.alves.oliver@gmail.com>
2023-02-15 15:07:54 -0300

    Add controller in the project

---
+++ OrderOrquestration.Application/Features/OrderOrquestration/Models/OrderOrquestrationInput.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using OrderOrquestration.Application.Shared.Domain.Models;
+
+namespace OrderOrquestration.Application.Features.OrderOrquestration.Models
+{
+    public class OrderOrquestrationInput : IRequest<Output<string>>
+    {
+        public string ProductOrder { get; set; }
+    }
+}
---
+++ OrderOrquestration.WebApi/Controllers/OrderOrquestrationController.cs
@@ -0,0 +1,45 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using OrderOrquestration.Application.Features.OrderOrquestration.Models;
+using OrderOrquestration.Application.Shared.Domain.Models;
+using System.ComponentModel.DataAnnotations;
+using System.Net;
+
+namespace OrderOrquestration.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [Produces("application/json")]
+    public class OrderOrquestrationController : Controller  
+    {
+        private ILogger<OrderOrquestrationController> _logger;
+        private readonly IMediator _mediator;
+
+        public OrderOrquestrationController(ILogger<OrderOrquestrationController> logger, IMediator mediator)
+        {
+            _logger = logger;
+            _mediator = mediator;
+        }
+
+        /// <summary>
+        /// Recebe a comunicação de um pagamento e então processa as ações de acordo com as especificações e regras do produto envolvido.
+        /// </summary>
+        /// <param name="productOrder"> produto envovido no pagamento</param>
+        /// <response code="200">Retorna o status do processamento das ações</response>
+        /// <response code="500">Internal Server Error</response>
+        [HttpPost()]
+        [ProducesResponseType(typeof(Output<string>), (int)HttpStatusCode.OK)]
+        [ProducesResponseType((int)HttpStatusCode.InternalServerError)]
+        public async Task<IActionResult> OrderOrquestrationControllerAsync([FromQuery][Required] string productOrder, CancellationToken cancellationToken)
+        {
+            try
+            {
+                var input = new OrderOrquestrationInput();
+                return Ok(await _mediator.Send(input, cancellationToken));
+            }
+            catch (Exception)
+            {
+                return StatusCode((int)HttpStatusCode.InternalServerError);
+            }
+        }
+    }
+}
